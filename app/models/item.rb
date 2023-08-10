999b89db87e1c03531397d2b0ceeab536ea6277e
RyumaKatoh <ryuma012645@gmail.com>
2023-08-10 20:27:10 +0900

    プルダウン作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
-  belongs_to :category
   # has_one :purchase
+
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :delivery_burden
+  belongs_to :prefecture
+  # belongs_to :shipping_duration
 end
