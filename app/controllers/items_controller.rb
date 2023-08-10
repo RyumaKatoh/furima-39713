ed566fb3101918bc2cdc1872cec26ae506860f63
RyumaKatoh <ryuma012645@gmail.com>
2023-08-10 15:41:42 +0900

    出品機能first-commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,10 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
+    @item = Item.new
+  end  
+
+  def new
+    @item = Item.new
   end  
 end
