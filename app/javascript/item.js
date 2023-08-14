86a45daa97274052f858098d7e1e131fe54f8015
RyumaKatoh <ryuma012645@gmail.com>
2023-08-14 20:52:15 +0900

    商品一覧機能　レビュー1回目

--- app/javascript/item.js
+++ app/javascript/item.js
@@ -1,12 +1,14 @@
 function item (){
   const itemPrice  = document.getElementById("item-price");
+  const taxPrice  = document.getElementById("add-tax-price");
+  const benefit = document.getElementById("profit");
   itemPrice.addEventListener("keyup", () => {
     const price = itemPrice.value;
     const priceItem = parseInt(price);
-    const taxPrice  = document.getElementById("add-tax-price");
+  
     const priceTax = `${Math.floor(priceItem/10)}`;
     taxPrice.innerHTML = `${priceTax}`;
-  const benefit = document.getElementById("profit");
+  
     const profit = `${priceItem - priceTax}`
     benefit.innerHTML = `${profit}`;
   });  
