5ada9a8b8c621c94c21aa669309421485305aefe
RyumaKatoh <ryuma012645@gmail.com>
2023-08-15 14:41:46 +0900

    詳細画面遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, only: [:new]
 
   def index
     @items = Item.includes(:user).order(created_at: :desc)
-    # @item = Item.find(params[:item_id])
+    # @item = Item.find(params[:id])
   end  
 
   def new
@@ -17,6 +17,10 @@ class ItemsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end  
+
+  def show 
+    @item = Item.find(params[:id])
   end  
 
   private
