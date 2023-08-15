1e861d6aa0f4b11c92fd7998079013bc6dc89426
RyumaKatoh <ryuma012645@gmail.com>
2023-08-15 17:08:25 +0900

    削除機能追加、set_itemメソッド導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order(created_at: :desc)
@@ -20,11 +21,9 @@ class ItemsController < ApplicationController
   end  
 
   def show 
-    @item = Item.find(params[:id])
   end  
 
   def edit
-    @item = Item.find(params[:id])
     if !user_signed_in?
       redirect_to new_user_session_path 
     elsif current_user != @item.user
@@ -33,7 +32,6 @@ class ItemsController < ApplicationController
   end
   
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -41,8 +39,20 @@ class ItemsController < ApplicationController
     end 
   end  
 
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to item_path(@item.id)
+    end
+  end  
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :item_detail, :category_id, :item_condition_id, :delivery_burden_id, :prefecture_id, :shipping_duration_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end  
 end
